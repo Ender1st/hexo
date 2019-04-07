92cf2c0e56a88466c32ce45b5e8aac87e092a16f
Ender1st <mr.shiyuanzhe@gmail.com>
2019-04-07 06:38:05 -0700

    publish

--- webhook.js
+++ webhook.js
@@ -1,11 +1,29 @@
-var http = require('http')
-var exec = require('child_process').exec
-
-http.createServer(function (req, res) {
-    // 该路径与WebHooks中的路径部分需要完全匹配，实现简易的授权认证。
-    if(req.url === '/webhooks/push/123456'){
-        // 如果url匹配，表示认证通过，则执行 sh ./deploy.sh
-        exec('sh ./deploy.sh')
-    }
-    res.end()
-}).listen(4002)
+﻿var http = require('http')
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/webhooks_push', secret: '20040526' })
+function run_cmd(cmd, args, callback) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var resp = "";
+  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
+  child.stdout.on('end', function() { callback (resp) });
+}
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref);
+    run_cmd('sh', ['./deploy.sh'], function(text){ console.log(text) });
+})
+try {
+  http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+      res.statusCode = 404
+      res.end('no such location')
+    })
+  }).listen(6666)
+}catch(err){
+  console.error('Error:', err.message)
+}
